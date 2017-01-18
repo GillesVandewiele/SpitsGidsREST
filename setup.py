e67fed2640e797777d94c9fbbe32faaeb3cf4663
GillesVandewiele <givdwiel.vandewiele@ugent.be>
2017-01-18 16:32:11 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='SpitsGidsREST',
+    version='',
+    packages=[''],
+    url='',
+    license='GPL-3.0',
+    author='Gilles Vandewiele',
+    author_email='',
+    description='',
+    install_requires=[
+          'pandas',
+          'flask',
+          'numpy',
+          'xgboost',
+          'sklearn',
+          'flask_restful'
+    ]
+)
